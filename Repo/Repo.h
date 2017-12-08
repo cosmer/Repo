4d6f46284aeacaa5f352a83bfbb6b17ffcafb80d
Charlie Osmer <charlie.osmer@gmail.com>
2017-12-07 19:47:39 -0500

    Remove RPCommitWalker

--- Repo/Repo.h
+++ Repo/Repo.h
@@ -33,7 +33,6 @@ FOUNDATION_EXPORT const unsigned char RepoVersionString[];
 #import <Repo/RPBlob.h>
 #import <Repo/RPReference.h>
 #import <Repo/RPRevWalker.h>
-#import <Repo/RPCommitWalker.h>
 #import <Repo/RPSignature.h>
 #import <Repo/RPStash.h>
 #import <Repo/RPSubmodule.h>
